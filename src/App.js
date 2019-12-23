6ce5c1a584c085d7e007dd9991a7508d46062bca
Serg8 <workintellect@gmail.com>
2019-12-23 23:25:29 +0300

    feat: learn withRouter

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ class App extends React.Component {
                     <Header />
                     <Sidebar />
                     <div className='content' >
-                        <Route path='/profile' render={ () => <ProfileContainer />} />
+                        <Route path='/profile/:userId?' render={ () => <ProfileContainer />} />
                         <Route path='/dialogs' render={ () => <DialogsContainer />} />
                         <Route path='/users' render={ () => <UsersContainer />} />
                         <Route path='/news' render={ () => <News />} />
