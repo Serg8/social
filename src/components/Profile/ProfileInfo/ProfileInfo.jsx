6c0f2adf0bb9ced3e45004d980f888999c6415e9
Sergey <workintellect@gmail.com>
2020-01-13 21:45:47 +0300

    feat: learn local state

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import s from './ProfileInfo.module.scss';
 import Preloader from "../../Common/Preloader/Preloader";
+import ProfileStatus from "./ProfileStatus";
 
 const ProfileInfo = (props) => {
 
@@ -11,10 +12,11 @@ const ProfileInfo = (props) => {
 
         return (
             <div>
-                <div className={s.avatar}>
-                    <img src={props.profile.photos.large} alt="" />
-                </div>
+                {/*<div className={s.avatar}>*/}
+                {/*    <img src={props.profile.photos.large} alt="" />*/}
+                {/*</div>*/}
                 <h2>{props.profile.fullName}</h2>
+                    <ProfileStatus status={"test"} />
                 <p>Обо мне: {props.profile.aboutMe}</p>
                 <p>Ищу работу: {props.profile.lookingForAJob ? 'да' : 'нет'}</p>
                 <p>Описание к поиску работы: {props.profile.lookingForAJobDescription}</p>
